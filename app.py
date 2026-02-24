f252e072f372d6a3771db115cb1b061ce12aef23
Oleg Anastassov <oleg.anastassov@gmail.com>
2026-02-24 22:59:59 +0100

    chore: add scaffolding for configs

--- app.py
+++ app.py
@@ -3,10 +3,30 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/health", methods=["GET"])
+@app.route('/', methods=['GET'])
+def index():
+    return health()
+
+
+@app.route('/health', methods=['GET'])
 def health():
     return {"status": "UP"}
 
 
-if __name__ == "__main__":
+@app.route('/configs/<service>/<environment>', methods=['GET'])
+def get_config(service, environment):
+    return f'GET(Config: {service}-{environment})'
+
+
+@app.route('/configs/<service>/<environment>', methods=['POST'])
+def save_config(service, environment):
+    return f'POST(Config: {service}-{environment})'
+
+
+@app.route('/configs/<service>/<environment>/versions', methods=['GET'])
+def list_versions(service, environment):
+    return f'GET(Versions: {service}-{environment})'
+
+
+if __name__ == '__main__':
     app.run(debug=True)
